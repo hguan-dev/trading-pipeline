ae729b536af0a03884144b83869b0340977345a4
Harry Guan <guanhuai2014@gmail.com>
2024-11-22 18:13:32 -0600

    all changes

--- src/pysrc/data_client.py
+++ src/pysrc/data_client.py
@@ -5,7 +5,7 @@ class DataClient:
     def _query_api(self) -> None:
         raise NotImplementedError
 
-    def _parse_message(self, message) -> None:
+    def _parse_message(self, message: str) -> None:
         raise NotImplementedError
 
     def get_data(self) -> None:
